1464acc6092cacdcedb4b1a87edc472050eed6a8
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-02-24 03:33:10 +0330

    commited

--- script.js
+++ script.js
@@ -104,7 +104,6 @@ const movieData = (movie) => {
   });
 
   movieSelectOpt();
-  
 };
 
 //* Add eventListener for clean input value
@@ -129,23 +128,21 @@ const movieSelectOpt = () => {
   });
 };
 
-
-
 // # Swiper
 
- var swiper = new Swiper(".mySwiper", {
-   effect: "coverflow",
-   grabCursor: true,
-   centeredSlides: true,
-   slidesPerView: "auto",
-   coverflowEffect: {
-     rotate: 50,
-     stretch: 0,
-     depth: 100,
-     modifier: 1,
-     slideShadows: true,
-   },
-   pagination: {
-     el: ".swiper-pagination",
-   },
- });
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
